845c88b913c4247ec336c72cac7bf57e42f3de6b
LX2010JY <LX2010JY@163.com>
2017-03-15 14:47:08 +0800

    '333'

--- algorithm/set/baseSet.js
+++ algorithm/set/baseSet.js
@@ -42,10 +42,70 @@ function Set() {
         }
         return keys;
     };
+    //并集操作
+    this.union = function (otherset) {
+        var unionSet = new Set();
+        var values = this.values();
+        for(let i=0;i<values.length;i++) {
+            unionSet.add(values[i]);
+        }
+        values = otherset.values();
+        for(let i=0;i<values.length;i++) {
+            unionSet.add(values[i]);
+        }
+        return unionSet;
+    };
+    //并集
+    this.intersection = function (otherset) {
+        var interset = new Set();
+        var values = otherset.values();
+        for(let i=0;i<values.length;i++) {
+            if(this.has(values[i])) {
+                interset.add(values[i]);
+            }
+        }
+        return interset;
+    };
+    //差集 A-(A和B的交集)
+    this.difference = function (otherset) {
+        var diffset = new Set();
+        var interset = this.intersection(otherset);
+        var values = this.values();
+        for(let i =0;i<values.length;i++) {
+            if(!interset.has(values[i])){
+                diffset.add(values[i]);
+            }
+        }
+        return diffset;
+    };
+    //判断是否是子集
+    this.issubset = function (otherset) {
+        if(this.size<otherset.size) {
+            return false;
+        } else {
+            var values = otherset.values();
+            for(let i=0;i<values.length;i++) {
+                if(!this.has(values[i])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
 }
 
 var set = new Set();
 set.add('a');
 set.add('b');
 set.add('3');
-console.log(set.values());
+var set2 = new Set();
+set2.add('asda');
+set2.add('1');
+set2.add('3');
+var set3 = new Set();
+set3.add('3');
+
+console.log("交集："+set.union(set2).values());
+console.log("并集："+set.intersection(set2).values());
+console.log("差集："+set.difference(set2).values());
+console.log(set.issubset(set3));
